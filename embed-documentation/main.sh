b09c827d5bc77cf70b530fdee9211a6835880264
Clément Sirieix <clementsirieix@clements-mbp.home>
2024-03-06 15:31:43 +0100

    feat: update embeddings setup

--- embed-documentation/main.sh
+++ embed-documentation/main.sh
@@ -5,14 +5,14 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 rm -rf ./documentation
 mkdir -p ./documentation
-git clone git@github.com:Chainlit/platform-docs.git
+git clone git@github.com:Chainlit/literal-docs.git
 
-FILE_PATHS=$(find ./platform-docs -name "*.mdx")
+FILE_PATHS=$(find ./literal-docs -name "*.mdx")
 
 while IFS= read -r file; do
     cp "$file" ./documentation/
 done <<< "$FILE_PATHS"
 
-rm -rf ./platform-docs
+rm -rf ./literal-docs
 
 python3 "$SCRIPT_DIR/main.py"
