39b4c672db7a5cad518e93aff011b34d5f9d6a7d
Hugues de Saxcé <hugues@chainlit.io>
2024-05-15 16:09:33 +0200

    fix: fix path

--- embed-documentation/main.sh
+++ embed-documentation/main.sh
@@ -4,12 +4,12 @@ SCRIPT=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
 
 rm -rf ./cookbooks ./documentation
-mkdir -p ./documentation ./documentation
+mkdir -p ./cookbooks ./documentation
 
 git clone git@github.com:Chainlit/literal-docs.git
 git clone git@github.com:Chainlit/literal-cookbook.git
 
-# Use *.mdx from documentation and README.md, *.py from cookbook
+# Use *.mdx from documentation and README.md, *.py and *.ts from cookbook
 find ./literal-docs -name "*.mdx" -exec bash -c 'newname="./documentation/$(echo {} | sed "s|/|_|g")"; cp "{}" "$newname"' \;
 find ./literal-cookbook \( -name "README.md" -o -name "*.py" -o -name "*.ts" \) -exec bash -c 'newname="./cookbooks/$(echo {} | sed "s|/|_|g")"; cp "{}" "$newname"' \;
 
